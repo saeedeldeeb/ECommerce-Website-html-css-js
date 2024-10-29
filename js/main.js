e4ccd6fbfaeb9f510e725f76998801b11ce6c0b0
eldeeb <saeedeldeeb1@gmail.com>
2024-10-30 00:05:05 +0300

    Added cart side menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// open & close cart
+
+const cart = document.querySelector('.cart');
+
+function openCart() {
+  cart.classList.add('active');
+}
+
+function closeCart() {
+  cart.classList.remove('active');
+}
