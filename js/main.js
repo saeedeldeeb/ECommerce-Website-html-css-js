dfb582b18d98c6e8a07ae622ce0cf739146c6bde
eldeeb <saeedeldeeb1@gmail.com>
2024-11-10 12:59:18 +0200

    Added back to top button

--- js/main.js
+++ js/main.js
@@ -69,3 +69,12 @@ function deleteItemCart(index) {
     });
   });
 }
+
+// back to top
+let back_to_top = document.querySelector('.back_to_top');
+back_to_top.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
